ac19e6bc48e3d6f96da1aa2e7f74931e877b7e72
mkoertshuis <mickkoertshuis@gmail.com>
2023-06-14 13:58:01 +0200

    Added the slow and fast data actions.

---
+++ js/data_fast.js
@@ -0,0 +1,172 @@
+data_fast = [
+    {
+        title: "Run",
+        icon: "run",
+        subtitle: "Run from one zone to a neighboring zone.",
+        description: "Run from one zone to a neighboring zone.",
+        reference: "CRB, pg. 89.",
+        bullets: [
+            "Or between SHORT and ENGAGED range from an enemy or PC in the same zone you are already in.",
+            "No roll required to run, unless it's a Cluttered zone (see page 83)."
+        ]
+    },
+    {
+        title: "Move through door/hatch",
+        icon: "guarded-tower",
+        subtitle: "You can open a door with a fast action",
+        description: "You can open an unlocked door or hatch with a fast action.",
+        reference: "CRB, pg. 89.",
+        bullets: [
+            "A locked door or hatch can be broken down. A typical metal door or hatch can take 10 points of damage before it gives in. More sturdy doors require more force, and also have an Armor Rating (see page 98).",
+            "In some cases, you can open an electronically locked door or hatch with a COMTECH roll."
+        ]
+    },
+    {
+        title: "Get up",
+        icon: "muscle-up",
+        subtitle: "",
+        description: "",
+        reference: "CRB, pg. xxx.",
+        bullets: [
+            
+        ]
+    },
+    {
+        title: "Draw weapon",
+        icon: "blunderbuss",
+        subtitle: "",
+        description: "",
+        reference: "CRB, pg. xxx.",
+        bullets: [
+            
+        ]
+    },
+    {
+        title: "Block attack",
+        icon: "shield-reflect",
+        subtitle: "Close Combat",
+        description: "If you are attacked in close combat, you can choose to block the attack, to avoid being hit.",
+        reference: "CRB, pg. 92.",
+        bullets: [
+            "Blocking is a fast action, and you roll for CLOSE COMBAT.",
+            "For each rolled succes you can choose to:",
+            "DECREASE DAMAGE: You remove on of the enemy's successes. If they are left with no sucesses, the attack misses. This effect can be chosen multiple times.",
+            "COUNTERATTACK: You perform a counterattack, dealing damage to the attacker equal to the Damage rating of your weapon. You cannot spend additional succes to increase the damage of your counterattack.",
+            "DISARM: You disarm your enemy.",
+            "Blocking breaks the initiative order in the Round. However, it does count against your two available actions in the Round. You can save your action for later in the Round.",
+            "If you are unarmed, you can only block unarmed attacks from other humans. To block an armed close combat attack, or an attack by a Xenomorph creature, you need to wield some kind of sturdy weapon or tool."
+        ]
+    },
+    {
+        title: "Pick up Item",
+        icon: "snatch",
+        subtitle: "",
+        description: "",
+        reference: "CRB, pg. xxx.",
+        bullets: [
+            
+        ]
+    },
+    {
+        title: "Shove",
+        icon: "slap",
+        subtitle: "Close Combat",
+        description: "Shove an opponent at ENGAGED range away from you.",
+        reference: "CRB, pg. 93.",
+        bullets: [
+            "The GM can modify your roll for certain opponents.",
+            "If you succeed, your opponent is shoved away to SHORT range from you.",
+            "Shoving a target away can be useful if you want to engage it in ranged combat rather than fight it hand-to-hand.",
+            "Shoves can be blocked."
+        ]
+    },
+    {
+        title: "Grapple attack",
+        icon: "grab",
+        subtitle: "Close Combat",
+        description: "",
+        reference: "CRB, pg. 93.",
+        bullets: [
+            "If you grapple your opponent as a stunt in close combat, both you and your opponent fall to the ground.",
+            "The opponent drops any weapon they were holding, and cannot move.",
+            "The only action they can perform is an attempt to break free--which is a slow action that succeeds if the opponent wins an opposed CLOSE COMBAT roll agains you.",
+            "While you are grappling, the only action you can perform is a grapple attack. This works as a normal unarmed attack, but is a fast action and cannot be blocked."
+        ]
+    },
+    {
+        title: "Retreat",
+        icon: "dodge",
+        subtitle: "Mobility",
+        description: "Move away from an ENGAGED enemy.",
+        reference: "CRB, pg. 93.",
+        bullets: [
+            "If you have an active enemy at ENGAGED range, you must make a MOBILITY roll to move away to SHORT range from them. If you fail, you still move but your enemy gets a free close combat attack against you. The free attack doesn't count toward their actions in the Round and you can't block it."
+        ]
+    },
+    {
+        title: "Aim",
+        icon: "targeting",
+        subtitle: "+2 to your attack roll.",
+        description: "If you take your time to aim carefully before squeezing the trigger, you get a +2 to your attack roll.",
+        reference: "CRB, pg. 94.",
+        bullets: [
+            "If you do anything else except shoot your weapon after you have aimed, or if you are hurt, you lose the effect of the aim and you need to spend another fast action to aim again."
+        ]
+    },
+    {
+        title: "Seek cover",
+        icon: "hood",
+        subtitle: "Before someone shoots at you.",
+        description: "Unlike blocking, you need to find cover before someone shoots at you.",
+        reference: "CRB, pg. 96.",
+        bullets: [
+            "Cover has an Armor Rating and works just like armor. When you're hit by a ranged attack, roll a number of Base Dice equal to the Armor rating. Each success you roll lowers the damage by one point. Cover and Armor can be combined, simply add the Armor ratings.",
+            "Shrubbery 2, Furniture 3, Door 4, Inner bulkhead 5, Outer bulkhead 6, Armored bulkhead 7+."
+        ]
+    },
+    {
+        title: "Assume overwatch position",
+        icon: "autogun",
+        subtitle: "Targeting a specific direction.",
+        description: "You can assume an overwatch position in a specified direction, as long as you have a ranged weapon and no enemies within ENGAGED range.",
+        reference: "CRB, pg. 97.",
+        bullets: [
+            "Between the time you assume the overwatch position and your time to act in the next Round, you can fire your weapon against a target in the chosen direction.",
+            "You can fire whenever you want in the turn order, and your shot is resolved before all other actions--even if they are already declared.",
+            "You must save your slow action in the Round for any overwatch attack you make.",
+            "If both you and an enemy assume overwatch positions against each other, then an opposed RANGED COMBAT roll determines which attack goes first. This roll does not count as an action for either of you.",
+            "You keep your overwatch position as long as you do nothing but shoot in the chosen direction.",
+            "It is lost if you perform any other action or: you are attacked in close combat or you suffer damage."
+        ]
+    },
+    {
+        title: "Grab the wheel",
+        icon: "cartwheel",
+        subtitle: "",
+        description: "",
+        reference: "CRB, pg. xxx.",
+        bullets: [
+            
+        ]
+    },
+    {
+        title: "Drive",
+        icon: "cartwheel",
+        subtitle: "Piloting",
+        description: "",
+        reference: "CRB, pg. xxx.",
+        bullets: [
+            
+        ]
+    },
+    {
+        title: "Use Item",
+        icon: "snatch",
+        subtitle: "Varies",
+        description: "",
+        reference: "CRB, pg. xxx.",
+        bullets: [
+            
+        ]
+    }
+]
---
+++ js/data_slow.js
@@ -0,0 +1,157 @@
+data_slow = [
+    {
+        title: "Crawl",
+        icon: "crawl",
+        subtitle: "If you are prone, you can't run.",
+        description: "Crawling works just like running, but it's a slow action.",
+        reference: "CRB, pg. 89.",
+        bullets: [
+            "You can't crawl twice in the same round.",
+            "In a Cramped zone, crawling is the only movement possible."
+        ]
+    },
+    {
+        title: "Close combat attack",
+        icon: "punch",
+        subtitle: "Close Combat",
+        description: "If you have an active enemy at ENGAGED range, you can't walk away from them. Instead, you must retreat (see page 93).",
+        reference: "CRB, pgs. 89, 92.",
+        bullets: [
+            "If your CLOSE COMBAT roll is successful, your attack hits and you inflict damage equal to the weapon's Damage rating on your opponent (see page 118).",
+            "Damage may be mitigated by armor (page 98).",
+            "STUNTS: For every extra success you roll, choose one of these stunts:\n-You inflict one additional point of damage. You can choose this stunt multiple times.\n-You out-maneuver your enemy and get to exchange your initiative score with them, taking effect next Round.\n-You knock or pull a weapon or other object from your opponent. You choose which.\n-Your opponent falls prone. This can only be used on humanoid opponents.\n-You pin your enemy in a tight clinch. This stunt can only be used on humans and synthetics."
+        ]
+    },
+    {
+        title: "Shoot firearm",
+        icon: "gunshot",
+        subtitle: "Ranged Combat",
+        description: "",
+        reference: "CRB, pg. 95.",
+        bullets: [
+            "If your attack hits, you inflict damage equal to the weapon's Damage rating on your opponent. For every extra success you roll, choose one of these stunts:\n-You inflict one additional point of damage. You can choose this stunt multiple times.\n-You pin down your enemy. PCs need to make an immediate Panic Roll. NPCs instead miss their next slow action.\n-You position yourself and get to exchange your initiative score with your enemy, taking effect next Round.\n-Your target drops a weapon or another hand-held object. You choose which.\n-Your opponent falls to the ground or is pushed back, for example through an airlock.",
+            "Modifiers: Aimed shot +2, Engaged range -3/+3, Short range 0, Medium range -1, Long range -2, Extreme range -3, Large target +2, Small target -2, Dim light -1, Darkness -2."
+        ]
+    },
+    {
+        title: "Burst of full auto fire",
+        icon: "bullets",
+        subtitle: "Ranged Combat",
+        description: "Weapons listed as fully automatic can fire long bursts of full auto fire.",
+        reference: "CRB, pg. 96.",
+        bullets: [
+            "Counts as a normal ranged attack, but with a few differences.",
+            "You get a +2 modification to your roll.",
+            "Your STRESS LEVEL immediately increases by one. Add the Stress Die to your roll for the full auto burst.",
+            "You can distribute any additional successes you roll beyond the first secondary targets withing SHORT range of the primary target. The first success assigned to a secondary target inflicts damage equal to the Damage rating of the weapons, and furter sucesses assigned to a secondary target can be used for stunts."
+        ]
+    },
+    {
+        title: "Throw weapon",
+        icon: "thrown-spear",
+        subtitle: "Ranged Combat",
+        description: "",
+        reference: "CRB, pg. xxx.",
+        bullets: [
+            
+        ]
+    },
+    {
+        title: "Reload",
+        icon: "ammo-box",
+        subtitle: "Reload your weapons",
+        description: "Most firearms in the world of ALIEN have large enough magazines that you don’t need to worry about counting individual bullets. However, when the tension rises, you risk wasting ammo and emptying your clip at the worst possible time.",
+        reference: "CRB, pg. 96.",
+        bullets: [
+            "Whenever you fire a weapon and roll one or more fails on your Stress Dice (see page 61), you have emptied your magazine after the attack and need to reload."
+        ]
+    },
+    {
+        title: "First aid",
+        icon: "first-aid",
+        subtitle: "Medical Aid",
+        description: "Help someone back onto their feet.",
+        reference: "CRB, pg. 98.",
+        bullets: [
+            "If the roll is successful, you get back up immediately, regaining Health equal to the number of successes in the skill roll.",
+            "Administering first aid to someone who isn't Broken has no effect.",
+            "Certain medical equipment can give bonusses to the roll (see Chapter 5)."
+        ]
+    },
+    {
+        title: "Stop panic",
+        icon: "screaming",
+        subtitle: "Command",
+        description: "When another character makes a Panic Roll and loses control, you can make a COMMAND roll to return them to their senses.",
+        reference: "CRB, pgs. 71, 104.",
+        bullets: [
+            "Panic also stops when you are Broken or One Turn passes."
+        ]
+    },
+    {
+        title: "Give orders",
+        icon: "pointing",
+        subtitle: "Command",
+        description: "You can bark orders to another character.",
+        reference: "CRB, pg. 71.",
+        bullets: [
+            "They must be able to hear you, even if via a comm radio.",
+            "For every success you roll, they get a +1 modification to their roll when carrying out the order you gave.",
+            "Officers and characters with the Pull Rank talent can use COMMAND to order other characters to do as they say."
+        ]
+    },
+    {
+        title: "Persuade",
+        icon: "telepathy",
+        subtitle: "Manipulation",
+        description: "Make another play do what you want or get attacked yourself.",
+        reference: "CRB, pg. 70.",
+        bullets: [
+            "Other player must make an opposed roll for MANIPULATION.",
+            "FAILURE: Your adversary won't listen and won't do what you want. They might start to dislike you, or even attack you if provoked.",
+            "SUCCESS: Your adversary must either do what you want or immedeiately attack you physically. Even if your adversary chooses to do what you want, they can still demand something in return.",
+            "STUNTS:\n-Your opponent does what you want without demanding a favor in return.\n-Your opponent does more than you ask for.\n-Your opponent is impressed by you and will try to help you later on in some way.",
+            "NPCs and other PCs can use MANIPULATION on you. If their roll succeeds, you must attack or offer a deal of some kind."
+        ]
+    },
+    {
+        title: "Use signature Item",
+        icon: "snatch",
+        subtitle: "Relieve stress",
+        description: "Once per Act in Cinematic play, and once per game session in Campaign play, you can interact with your signature item in some way to reduce your STRESS LEVEL one step.",
+        reference: "CRB, pg. 104.",
+        bullets: [
+            "Some conditions, such as Starving or Freezing (see page 106), can block your ability to relieve stress."
+        ]
+    },
+    {
+        title: "Climb into space suit",
+        icon: "space-suit",
+        subtitle: "Mobility",
+        description: "",
+        reference: "CRB, pg. xxx.",
+        bullets: [
+            
+        ]
+    },
+    {
+        title: "Enter/exit vehicle",
+        icon: "cartwheel",
+        subtitle: "",
+        description: "",
+        reference: "CRB, pg. xxx.",
+        bullets: [
+            
+        ]
+    },
+    {
+        title: "Start engine",
+        icon: "auto-repair",
+        subtitle: "",
+        description: "",
+        reference: "CRB, pg. xxx.",
+        bullets: [
+            
+        ]
+    }
+]
